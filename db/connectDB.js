6e0ac7320613dc4a2efd71ea6c0c2d57f5d0b5a0
baruch yankvtz <baruchyankvtz@baruchs-MacBook-Air.local>
2024-10-31 11:42:36 +0200

    Removed deprecated MongoDB options from connectDB

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         console.log('mongo_uri: ', process.env.MONGO_URI); 
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`db connected: ${conn.connection.host}`); 
     } catch (error) {
         console.log('error connecting to db: ', error.message); 
